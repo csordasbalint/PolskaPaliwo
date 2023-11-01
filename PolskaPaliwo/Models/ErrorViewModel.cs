f5670d183660d43c2a15e760f3a9f69a5aba2542
Csordás Bálint <balintcsordaas@gmail.com>
2023-11-01 17:54:14 +0100

    Add project files.

---
+++ PolskaPaliwo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PolskaPaliwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
