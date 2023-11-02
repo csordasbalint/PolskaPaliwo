e9d07bd81aefce4f0cb5e11b4fa90ed63c8f1c2c
Csordás Bálint <balintcsordaas@gmail.com>
2023-11-02 21:20:14 +0100

    Added repo layer

---
+++ PolskaPaliwo/Repository/CarAdRepository.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using PolskaPaliwo.Models;
+
+namespace PolskaPaliwo.Repository
+{
+    public class CarAdRepository : ICarAdRepository
+    {
+        private readonly IMongoCollection<CarAd> _carAds;
+
+        public CarAdRepository(IMongoDatabase database)
+        {
+            _carAds = database.GetCollection<CarAd>("CarAds");
+        }
+
+        public List<CarAd> GetAllCarAds()
+        {
+            return _carAds.Find(Builders<CarAd>.Filter.Empty).ToList();
+        }
+
+        public CarAd GetCarById(string id)
+        {
+            var filter = Builders<CarAd>.Filter.Eq(x => x.Id, id);
+            return _carAds.Find(filter).FirstOrDefault();
+        }
+
+        public void CreateCar(CarAd carAd)
+        {
+            _carAds.InsertOne(carAd);
+        }
+
+        public void UpdateCar(string id, CarAd carAd)
+        {
+            _carAds.ReplaceOne(c => c.Id == id, carAd);
+        }
+
+        public void DeleteCar(string id)
+        {
+            var objectId = new ObjectId(id);
+            var filter = Builders<CarAd>.Filter.Eq("Id", objectId);
+            _carAds.DeleteOne(filter);
+        }
+        
+    }
+}
---
+++ PolskaPaliwo/Repository/ICarAdRepository.cs
@@ -0,0 +1,13 @@
+﻿using PolskaPaliwo.Models;
+
+namespace PolskaPaliwo.Repository
+{
+    public interface ICarAdRepository
+    {
+        List<CarAd> GetAllCarAds();
+        CarAd GetCarById(string id);
+        void CreateCar(CarAd carAd);
+        void UpdateCar(string id, CarAd carAd);
+        void DeleteCar(string id);
+    }
+}
