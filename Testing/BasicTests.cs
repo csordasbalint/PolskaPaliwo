64c3385b1e5564677d6d47ab53f3fcf9ee5ef3b6
Csordás Bálint <balintcsordaas@gmail.com>
2023-11-21 23:02:45 +0100

    Added basic tests

---
+++ Testing/BasicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public BasicTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("Home/Index")]
+        [InlineData("Home/Privacy")]
+        public async Task BasicEndpointsCheck(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
