80f2c32eb910b6ae99d4baa303673d1b13fc0e80
Csordás Bálint <balintcsordaas@gmail.com>
2023-11-23 15:15:35 +0100

    Added tests for UserListings

--- Testing/AuthorizationTests.cs
+++ Testing/AuthorizationTests.cs
@@ -34,7 +34,7 @@ namespace Testing
 
         //Accessing CreateForm with athenticated and unathenticated user
         [Fact]
-        public void AuthenticatedUser_CanAccessCreateFormV()
+        public void AuthenticatedUser_CanAccessCreateForm()
         {
             // Arrange
             var carAdRepositoryMock = new Mock<ICarAdRepository>();
@@ -83,8 +83,61 @@ namespace Testing
 
 
 
+        [Fact]
+        public void AuthenticatedUser_CanAccessUserListings()
+        {
+            // Arrange
+            var carAdRepositoryMock = new Mock<ICarAdRepository>();
+            var userManagerMock = new Mock<UserManager<ApplicationUser>>(
+                Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
+
+            var expectedUserId = "testUserId";
+
+            var carAds = new List<CarAd>
+            {
+                new CarAd { CreatorId = expectedUserId },
+            };
+            carAdRepositoryMock.Setup(m => m.GetAllCarAds()).Returns(carAds);
+            
+            userManagerMock.Setup(m => m.GetUserId(It.IsAny<ClaimsPrincipal>()))
+                           .Returns(expectedUserId); // Return the expected user ID
+
+            var controller = new CarAdController(carAdRepositoryMock.Object, userManagerMock.Object);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, expectedUserId),
+            }, "mock"));
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var result = controller.UserListings() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("UserListingsView", result.ViewName);
+        }
 
 
+        [Fact]
+        public async Task UnauthenticatedUser_CannotAccessUserListings()
+        {
+            // Arrange
+            // Not needed - any request sent by _client is unauthentication on default
 
+            // Act
+            var response = await _client.GetAsync("/CarAd/UserListings");
+            var expectedRedirectUrl = "/Identity/Account/Login";
+            var redirectedUrl = response.Headers.Location?.OriginalString;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode); //302 - redirect code
+            Assert.Contains(expectedRedirectUrl, redirectedUrl); //asks for login
+        }
     }
 }
