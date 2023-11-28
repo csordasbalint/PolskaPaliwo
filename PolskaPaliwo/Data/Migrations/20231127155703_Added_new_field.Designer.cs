c54ce42c94295fe5cdcc80fa42791912a1e7097e
Csordás Bálint <balintcsordaas@gmail.com>
2023-11-28 11:55:14 +0100

    Added new migration - new field

--- PolskaPaliwo/Data/Migrations/20231127155703_Added_new_field.Designer.cs
+++ PolskaPaliwo/Data/Migrations/20231127155703_Added_new_field.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PolskaPaliwo.Data;
 
@@ -11,9 +12,10 @@ using PolskaPaliwo.Data;
 namespace PolskaPaliwo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231127155703_Added_new_field")]
+    partial class Added_new_field
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PolskaPaliwo/Data/Migrations/20231127155703_Added_new_field.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PolskaPaliwo.Data.Migrations
+{
+    public partial class Added_new_field : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastFeedback",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastFeedback",
+                table: "AspNetUsers");
+        }
+    }
+}
